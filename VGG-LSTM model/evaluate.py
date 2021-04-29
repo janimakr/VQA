7cff3fe5c4d0dd1294b57c5135e4c110451e764e
janimakr <janimakr580@gmail.com>
2021-04-29 10:50:08 +0530

    final evaluate

--- VGG-LSTM model/evaluate.py
+++ VGG-LSTM model/evaluate.py
@@ -5,76 +5,61 @@ from keras.preprocessing.sequence import pad_sequences
 from keras.models import load_model
 from nltk.translate.bleu_score import corpus_bleu
  
-# load doc into memory
-def load_doc(filename):
-	# open the file as read only
+# load the test images into memory as read only 
+def load_test_img(filename):
 	file=open(filename, 'r')
-	# read all text
 	text=file.read()
-	# close the file
 	file.close()
 	return text
  
-# load a pre-defined list of photo identifiers
-def load_set(filename):
-	doc=load_doc(filename)
-	dataset=list()
-	# process line by line
-	for line in doc.split('\n'):
-		# skip empty lines
+# load list of photo identifiers to process them line by line to a list and get img id 
+def load_img_id(filename):
+	document=load_test_img(filename)
+	data=list()
+	for line in document.split('\n'):
 		if len(line)<1:
 			continue
-		# get the image identifier
 		identifier=line.split('.')[0]
-		dataset.append(identifier)
-	return set(dataset)
+		data.append(identifier)
+	return set(data)
  
 # load clean descriptions into memory
-def load_clean_descriptions(filename, dataset):
-	# load document
-	doc=load_doc(filename)
+def load_clean_descriptions(file, data):
+	document=load_test_img(file)
 	descriptions=dict()
-	for line in doc.split('\n'):
-		# split line by white space
+	for line in document.split('\n'):
 		tokens=line.split()
-		# split id from description
-		image_id,image_desc=tokens[0],tokens[1:]
-		# skip images not in the set
-		if image_id in dataset:
-			# create list
-			if image_id not in descriptions:
-				descriptions[image_id]=list()
-			# wrap description in tokens
-			desc='startseq '+' '.join(image_desc)+' endseq'
-			# store
-			descriptions[image_id].append(desc)
+		img_id,img_description=tokens[0],tokens[1:]
+		if img_id in data:
+			if img_id not in descriptions:
+				descriptions[img_id]=list()
+			desc='startseq '+' '.join(img_description)+' endseq'
+			descriptions[img_id].append(desc)
 	return descriptions
  
-# load photo features
-def load_photo_features(filename, dataset):
-	# load all features
-	all_features=load(open(filename, 'rb'))
-	# filter features
-	features={k: all_features[k] for k in dataset}
+# load photo features and filter to only those present in dataset
+def load_photo_features(file, data):
+	full_features=load(open(file, 'rb'))
+	features={k: full_features[k] for k in data}
 	return features
  
 # convert a dictionary of clean descriptions to a list of descriptions
-def to_lines(descriptions):
-	all_desc=list()
+def to_list(descriptions):
+	all_descriptions=list()
 	for key in descriptions.keys():
-		[all_desc.append(d) for d in descriptions[key]]
-	return all_desc
+		[all_descriptions.append(d) for d in descriptions[key]]
+	return all_descriptions
  
 # fit a tokenizer given caption descriptions
 def create_tokenizer(descriptions):
-	lines=to_lines(descriptions)
+	lines=to_list(descriptions)
 	tokenizer=Tokenizer()
 	tokenizer.fit_on_texts(lines)
 	return tokenizer
  
 # calculate the length of the description with the most words
 def max_length(descriptions):
-	lines=to_lines(descriptions)
+	lines=to_list(descriptions)
 	return max(len(d.split()) for d in lines)
  
 # map an integer to a word
@@ -84,81 +69,57 @@ def word_for_id(integer, tokenizer):
 			return word
 	return None
  
-# generate a description for an image
+# generate a description for an image by iterating full length
 def generate_desc(model, tokenizer, photo, max_length):
-	# seed the generation process
 	in_text='startseq'
-	# iterate over the whole length of the sequence
 	for i in range(max_length):
-		# integer encode input sequence
 		sequence=tokenizer.texts_to_sequences([in_text])[0]
-		# pad input
 		sequence=pad_sequences([sequence], maxlen=max_length)
-		# predict next word
 		yhat=model.predict([photo,sequence], verbose=0)
-		# convert probability to integer
 		yhat=argmax(yhat)
-		# map integer to word
 		word=word_for_id(yhat, tokenizer)
-		# stop if we cannot map the word
 		if word is None:
 			break
-		# append as input for generating the next word
 		in_text+=' '+word
-		# stop if we predict the end of the sequence
 		if word=='endseq':
 			break
 	return in_text
  
-# evaluate the skill of the model
+# evaluate the skill of the model by storing actual and predicted
 def evaluate_model(model, descriptions, photos, tokenizer, max_length):
 	actual,predicted=list(),list()
-	# step over the whole set
 	for key, desc_list in descriptions.items():
-		# generate description
 		yhat=generate_desc(model, tokenizer, photos[key], max_length)
-		# store actual and predicted
 		references=[d.split() for d in desc_list]
 		actual.append(references)
 		predicted.append(yhat.split())
-	# calculate BLEU score
+# calculate BLEU score
 	print('BLEU-1: %f' % corpus_bleu(actual, predicted, weights=(1.0, 0, 0, 0)))
 	print('BLEU-2: %f' % corpus_bleu(actual, predicted, weights=(0.5, 0.5, 0, 0)))
 	print('BLEU-3: %f' % corpus_bleu(actual, predicted, weights=(0.3, 0.3, 0.3, 0)))
 	print('BLEU-4: %f' % corpus_bleu(actual, predicted, weights=(0.25, 0.25, 0.25, 0.25)))
  
-# prepare tokenizer on train set
- 
-# load training dataset (6K)
+
 filename='D:\Jenny-work\Projects\Open Lab\Open Lab\Flickr8k_text\Flickr_8k.trainImages.txt'
-train=load_set(filename)
+train=load_img_id(filename)
 print('Dataset: %d'%len(train))
-# descriptions
 train_descriptions=load_clean_descriptions('descriptions.txt', train)
 print('Descriptions: train=%d' % len(train_descriptions))
-# prepare tokenizer
 tokenizer=create_tokenizer(train_descriptions)
 vocab_size=len(tokenizer.word_index) + 1
 print('Vocabulary Size: %d' % vocab_size)
-# determine the maximum sequence length
 max_length=max_length(train_descriptions)
 print('Description Length: %d' % max_length)
- 
-# prepare test set
- 
-# load test set
+
 filename='D:\Jenny-work\Projects\Open Lab\Open Lab\Flickr8k_text\Flickr_8k.testImages.txt'
-test=load_set(filename)
+test=load_img_id(filename)
 print('Dataset: %d' % len(test))
-# descriptions
 test_descriptions=load_clean_descriptions('descriptions.txt', test)
 print('Descriptions: test=%d' % len(test_descriptions))
-# photo features
 test_features=load_photo_features('features.pkl', test)
 print('Photos: test=%d' % len(test_features))
  
-# load the model
+# load the model and evaluate
 filename='model_13.h5'
 model=load_model(filename)
-# evaluate model
 evaluate_model(model, test_descriptions, test_features, tokenizer, max_length)

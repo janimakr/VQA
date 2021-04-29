d55f8cc012ba300a410f0316dea38b999cc229a0
janimakr <janimakr580@gmail.com>
2021-04-29 10:29:22 +0530

    edits

--- VGG-LSTM model/text_prepro.py
+++ VGG-LSTM model/text_prepro.py
@@ -1,26 +1,26 @@
 import string
-# extracting the contents of a file and returning to the user
-def load_doc(filename):
-	file=open(filename, 'r')
-	text=file.read()
+# extracting the contents of a file and returning to the user  -text in token form 
+def get_tokens(datasettokens):
+	file=open(datasettokens, 'r')
+	tokens=file.read()
 	file.close()
-	return text
+	return tokens
  
 # for each image traverse through the document and create a dictionary where,
 #  key is image id and the values are the descriptions of the image
-def load_descriptions(doc):
-	mapping=dict()
-	for line in doc.split('\n'):
+def create_dictionary(document):
+	connect=dict()
+	for line in document.split('\n'):
 		tokens=line.split()
 		if len(line) < 2:
 			continue
-		image_id,image_desc=tokens[0],tokens[1:]
-		image_id=image_id.split('.')[0]
-		image_desc=' '.join(image_desc)
-		if image_id not in mapping:
-			mapping[image_id]=list()
-		mapping[image_id].append(image_desc)
-	return mapping
+		img_id,img_description=tokens[0],tokens[1:]
+		img_id=img_id.split('.')[0]
+		img_description=' '.join(img_description)
+		if img_id not in connect:
+			connect[img_id]=list()
+		connect[img_id].append(img_description)
+	return connect
 
 # picking each word in the descriptions and cleaning the text by converting all to lower case,
 # removing the punctuations and single character words like 's','a' etc.
@@ -37,7 +37,7 @@ def clean_descriptions(descriptions):
  
 # convert the loaded descriptions into a vocabulary of words
 # identifying the corpus/dictionary for the application
-def to_vocabulary(descriptions):
+def vocab_of_words(descriptions):
 	all_desc=set()
 	for key in descriptions.keys():
 		[all_desc.update(d.split()) for d in descriptions[key]]
@@ -54,11 +54,11 @@ def save_descriptions(descriptions, filename):
 	file.write(data)
 	file.close()
  
-filename='D:\Jenny-work\Projects\Open Lab\Open Lab\Flickr8k_text\Flickr8k.token.txt'
-doc=load_doc(filename)
-descriptions=load_descriptions(doc)
-print('Loaded: %d ' % len(descriptions))
+datasettokens='D:\Jenny-work\Projects\Open Lab\Open Lab\Flickr8k_text\Flickr8k.token.txt'
+tokens=get_tokens(datasettokens)
+descriptions=create_dictionary(tokens)
+print('Loaded Descriptions: %d ' % len(descriptions))
 clean_descriptions(descriptions)
-vocabulary=to_vocabulary(descriptions)
-print('Vocabulary Size: %d' % len(vocabulary))
+vocab=vocab_of_words(descriptions)
+print('Vocabulary Size: %d' % len(vocab))
 save_descriptions(descriptions, 'descriptions.txt')

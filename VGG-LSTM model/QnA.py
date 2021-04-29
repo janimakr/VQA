a01acb0fc6771c6d2f1baed1cb48da6c7c3db607
Vishnu K <46495295+78vk@users.noreply.github.com>
2021-04-29 10:28:38 +0530

    Final update

--- VGG-LSTM model/QnA.py
+++ VGG-LSTM model/QnA.py
@@ -1,11 +1,9 @@
 from transformers import pipeline
-question_answering = pipeline('question-answering')
 
+qna=pipeline('question-answering')
 with open('inputfromDescriptionModel.txt','r') as file:
-    context = file.read()
-
-
-question = input("Ask your question! \n")
-result = question_answering(question=question, context=context)
-print("Answer:", result['answer'])
-# print("Score:", result['score'])
+    des=file.read()
+ques=input("Ask your question! \n")
+Ans=qna(question=ques,context=des)
+print("Answer: ", Ans['answer'])
+# print("Score:", Ans['score'])
--- VGG-LSTM model/generate.py
+++ VGG-LSTM model/generate.py
@@ -2,59 +2,59 @@ from keras.preprocessing.text import Tokenizer
 from pickle import dump
 
 # extracting the contents of a file and returning to the user
-def load_doc(filename):
-	file=open(filename, 'r')
-	text=file.read()
-	file.close()
-	return text
+def loadFile(Fname):
+	f=open(Fname, 'r')
+	txt=f.read()
+	f.close()
+	return txt
 
 #  extract the names of the files
 #  and store in the list dataset 
-def load_set(filename):
-	doc=load_doc(filename)
-	dataset=list()
-	for line in doc.split('\n'):
-		if len(line)<1:
+def loading(Fname):
+	doc=loadFile(Fname)
+	dset=list()
+	for perline in doc.split('\n'):
+		if len(perline)<1:
 			continue
-		identifier=line.split('.')[0]
-		dataset.append(identifier)
-	return set(dataset)
+		id=perline.split('.')[0]
+		dset.append(id)
+	return set(dset)
 
 # create a dictionary of the image:descriptions from the images selected for 
 # training and add a atart and end pointer: startseq,endseq 
-def load_clean_descriptions(filename, dataset):
-	doc=load_doc(filename)
-	descriptions=dict()
-	for line in doc.split('\n'):
-		tokens=line.split()
-		image_id,image_desc=tokens[0],tokens[1:]
-		if image_id in dataset:
-			if image_id not in descriptions:
-				descriptions[image_id]=list()
-			desc='startseq '+' '.join(image_desc) +' endseq'
-			descriptions[image_id].append(desc)
-	return descriptions
+def loadCleanDescriptions(Fname, dset):
+	doc=loadFile(Fname)
+	desc=dict()
+	for perline in doc.split('\n'):
+		token=perline.split()
+		imgid,img_desc=token[0],token[1:]
+		if imgid in dset:
+			if imgid not in desc:
+				desc[imgid]=list()
+			desc='startseq '+' '.join(img_desc) +' endseq'
+			desc[imgid].append(desc)
+	return desc
 
-# convert a dictionary of clean descriptions to a list of descriptions,
+
 #  appending each description to a list
-def to_lines(descriptions):
-	all_desc=list()
-	for key in descriptions.keys():
-		[all_desc.append(d) for d in descriptions[key]]
-	return all_desc
+def toLines(desc):
+	allDesc=list()
+	for key in desc.keys():
+		[allDesc.append(d) for d in desc[key]]
+	return allDesc
 
 # fit a tokenizer on the descriptions and maintain tokenization stats
-def create_tokenizer(descriptions):
-	lines=to_lines(descriptions)
-	tokenizer=Tokenizer()
-	tokenizer.fit_on_texts(lines)
-	return tokenizer
+def Tokenisation(desc):
+	l=toLines(desc)
+	tizer=Tokenizer()
+	tizer.fit_on_texts(l)
+	return tizer
 
-# load training dataset (6K images)
-filename='D:\Jenny-work\Projects\Open Lab\Open Lab\Flickr8k_text\Flickr_8k.trainImages.txt'
-train=load_set(filename)
+
+Fname='D:\Jenny-work\Projects\Open Lab\Open Lab\Flickr8k_text\Flickr_8k.trainImages.txt'
+train=loading(Fname)
 print('Dataset: %d' % len(train))
-train_descriptions=load_clean_descriptions('descriptions.txt', train)
-print('Descriptions: train=%d' % len(train_descriptions))
-tokenizer=create_tokenizer(train_descriptions)
-dump(tokenizer, open('tokenizer.pkl', 'wb'))
+train_desc=loadCleanDescriptions('descriptions.txt', train)
+print('Descriptions: train=%d' % len(train_desc))
+tizer=Tokenisation(train_desc)
+dump(tizer, open('tokenizer.pkl', 'wb'))
